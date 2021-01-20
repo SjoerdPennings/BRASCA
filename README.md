1629324a97185a52d944e3beacba174434aa5f92
SjoerdPennings <sjoerdjepennings@gmail.com>
2021-01-20 15:08:16 +0100

    Added stack manipulation

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # BRASCA
 
 **BRASCA - BRAckets and other Symbols turning Code into Ascii**
-
 `i tried my best with the acronym, okay`
 
 ## Data
@@ -52,21 +51,21 @@
 
 |Operator|Function|Implemented|
 |-------|-------|------|
-|`a`|Pop the stack, push it to register A|
-|`A`|Pop register A, push it to the stack|
-|`b`|Pop the stack, push it to register B|
-|`B`|Pop register B, push it to the stack|
-|`,`|Reverse stack|
-|`:`|Duplicate TOS|
-|`;`|Duplicate BOS|
-|`m`|Pop n from TOS and push the top n items on the stack to BOS|
-|`M`|Pop n from BOS and push the bottom n items on the stack to TOS|
-|`?`|Push a random number from 0 to 127 to the stack|
-|`!`|Push the length of the stack to the stack|
-|`$`|Swap the top two items on the stack|
-|`S`|Pop A,B from the stack, concatenate B and A, push the result to stack|
-|`x`|Pop TOS and discard it|
-|`X`|Pop BOS and discard it|
+|`a`|Pop the stack, push it to register A|Yes|
+|`A`|Pop register A, push it to the stack|Yes|
+|`b`|Pop the stack, push it to register B|Yes|
+|`B`|Pop register B, push it to the stack|Yes|
+|`,`|Reverse stack|Yes|
+|`:`|Duplicate TOS|Yes|
+|`;`|Duplicate BOS|Yes|
+|`m`|Pop n from TOS and push the top n items on the stack to BOS|Yes|
+|`M`|Pop n from BOS and push the bottom n items on the stack to TOS|Yes|
+|`?`|Push a random number from 0 to 127 to the stack|Yes|
+|`!`|Push the length of the stack to the stack|Yes|
+|`$`|Swap the top two items on the stack|Yes|
+|`S`|Pop A,B from the stack, concatenate B and A, push the result to stack|Yes|
+|`x`|Pop TOS and discard it|Yes|
+|`X`|Pop BOS and discard it|Yes|
 
 ### I/O
 
@@ -89,6 +88,6 @@
 |`=`|Pop A,B from the stack, push 1 to the stack if B == A, else push 0 to the stack|
 |`@`|Terminate program without implicit output.|
 |`#`|Pop A from the stack, if A > 0, execute the next instruction, otherwise skip it.|
-|`[...]`    |Pop A from the stack, if A is non-zero execute the code inside \[\], then jump back to the \[ and repeat, else jump to \] and continue on|
+|`[...]`    |Check, but don't pop, the top of the stack. If it is non-zero execute the code inside \[\], then jump back to the \[ and repeat, else jump to \] and continue on|Yes|
 |`j`|Pop n from the stack, move the code pointer n cells backwards|
 |`J`|Pop n from the stack, move the code pointer n cells forwards|
--- brasca.py
+++ brasca.py
@@ -1,6 +1,6 @@
 import sys, getch, math
 from brasca_class import BrascaClass
-
+from random import randrange
 
 """
 System arguments:
@@ -64,8 +64,6 @@ if __name__ == "__main__":
                     brasca.push_number(8)
                 elif command == "9":
                     brasca.push_number(9)
-
-                #Push number shortcuts
                 elif command == "l":
                     brasca.push_number(10)
                 elif command == "L":
@@ -93,51 +91,111 @@ if __name__ == "__main__":
                     brasca.code_pointer += 1
 
                 #Math operators
-                elif command == "+":
+                elif command == "+": #Add
                     a = brasca.pop()
                     b = brasca.pop()
                     brasca.push_number(b+a)
-                elif command == "-":
+                elif command == "-": #Subtract
                     a = brasca.pop()
                     b = brasca.pop()
                     brasca.push_number(b-a)
-                elif command == "*":
+                elif command == "*": #Multiply
                     a = brasca.pop()
                     b = brasca.pop()
                     brasca.push_number(b*a)
-                elif command == "/":
+                elif command == "/": #Integer division
                     a = brasca.pop()
                     b = brasca.pop()
                     brasca.push_number(b//a)
-                elif command == "%":
+                elif command == "%": #Modulo
                     a = brasca.pop()
                     b = brasca.pop()
                     brasca.push_number(b%a)
-                elif command == "^":
+                elif command == "^": #Exponent
                     a = brasca.pop()
                     b = brasca.pop()
                     brasca.push_number(b**a)
-                elif command == "s":
+                elif command == "s": #Sqrt
                     a = brasca.pop()
                     brasca.push_number(math.floor(math.sqrt(a)))
 
                 #Bitwise operators
-                elif command == "~":
+                elif command == "~": #NOT
                     a = brasca.pop()
                     brasca.push_number(~a)
-                elif command == "&":
+                elif command == "&": #AND
                     a = brasca.pop()
                     b = brasca.pop()
                     brasca.push_number(b&a)
-                elif command == "|":
+                elif command == "|": #OR
                     a = brasca.pop()
                     b = brasca.pop()
                     brasca.push_number(b|a)
-                elif command == "_":
+                elif command == "_": #XOR
                     a = brasca.pop()
                     b = brasca.pop()
                     brasca.push_number(b^a)
 
+
+                #Stack
+                elif command == "a": #push to register A
+                    brasca.set_register("a")
+                elif command == "b": #push to register B
+                    brasca.set_register("b")
+                elif command == "A": #pop from register A
+                    brasca.read_register("a")
+                elif command == "B": #pop from register B
+                    brasca.read_register("b")
+                elif command == ",": #reverse stack
+                    brasca.reverse()
+                elif command == ":": #duplicate top of stack
+                    a = brasca.pop()
+                    brasca.push_number(a)
+                    brasca.push_number(a)
+                elif command == ";": #duplicate bottom of stack
+                    a = brasca.pop_bottom()
+                    brasca.push_number_bottom(a)
+                    brasca.push_number_bottom(a)
+                elif command == "m": #move top of stack to bottom n times
+                    amount = brasca.pop()
+                    for n in range(0,amount):
+                        value = brasca.pop()
+                        brasca.push_number_bottom(value)
+                elif command == "M": #move bottom of stack to top n times
+                    amount = brasca.pop()
+                    for n in range(0,amount):
+                        value = brasca.pop_bottom()
+                        brasca.push_number(value)
+                elif command == "?": #random number 0-127
+                    brasca.push_number(randrange(128))
+                elif command == "!": #push length of stack
+                    brasca.push_number(brasca.length())
+                elif command == "$": #swap top two items on stack
+                    a = brasca.pop()
+                    b = brasca.pop()
+                    brasca.push_number(a)
+                    brasca.push_number(b)
+                elif command == "S": #concatenate top two numbers
+                    a = brasca.pop()
+                    b = brasca.pop()
+                    brasca.push_number(int(str(b)+str(a)))
+                elif command == "x": #discard top of stack
+                    brasca.pop()
+                elif command == "x": #discard bottom of stack
+                    brasca.pop_bottom()
+
+                #I/O
+
+
+                #Control Flow
+
+                
+                elif command == "[": #While-loop start
+                    if brasca.stack[-1] == 0:
+                        brasca.code_pointer = brasca.while_loops[brasca.code_pointer]
+                elif command == "]": #While-loop end
+                    if brasca.stack[-1] != 0 and brasca.stack != []:
+                        brasca.code_pointer = brasca.while_loops[brasca.code_pointer]
 
             #If the interpreter is reading as a string
             elif brasca.string_mode == True:
